b0f4fb637395924d27c1dd9ba592b5ceb8e02aa3
AndoGhevian <lenyvonbeer@gmail.com>
2020-11-29 09:11:03 +0400

    My new Test branch

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as embed } from './Embed'
 export { default as race } from './Race'
 export { default as sync } from './Sync'
 
-console.log('helloaaaaaaaaaaaaaaaa')
+console.log('helloaaaaaaaaaaaaaaa222222222222222a')
