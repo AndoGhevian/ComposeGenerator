3da0b63bd8eee62f68e6a14ce159f977993551d1
AndoGhevian <lenyvonbeer@gmail.com>
2020-12-02 19:01:51 +0400

    Comment Tests

--- src/index.ts
+++ src/index.ts
@@ -11,77 +11,65 @@ export {
 } from './commonTypes'
 
 
-import sync from './Sync'
-import embed from './Embed'
-import race from './Race'
-import { inspect } from 'util'
+// import sync from './Sync'
+// import embed from './Embed'
+// import race from './Race'
+// import { inspect } from 'util'
 
-const compose = embed({
-    outer: function* () {
-        yield 1
-        yield 2
-        yield 3
-        yield 4
-    },
-    inner: sync({
-        fir: function* () {
-            yield 1
-            yield 2
-            yield 3
-            yield 4
-            return 5
-        },
-        sec: function* () {
-            yield 'Welcome Andy 1'
-            yield 'welcome Andy 2'
-            return 'welcome Andy 3'
-        },
-        third: function* (welcome: string) {
-            yield `Welcome ${welcome ? welcome : 'default'} 1`
-            yield `Welcome ${welcome ? welcome : 'default'} 2`
-            return `Welcome ${welcome ? welcome : 'default'} 3`
-        }
-    }),
-})
+// const compose = embed({
+//     outer: function* () {
+//         yield 1
+//         yield 2
+//         yield 3
+//         yield 4
+//     },
+//     inner: sync({
+//         fir: function* () {
+//             yield 1
+//             yield 2
+//             yield 3
+//             yield 4
+//             return 5
+//         },
+//         sec: function* () {
+//             yield 'Welcome Andy 1'
+//             yield 'welcome Andy 2'
+//             return 'welcome Andy 3'
+//         },
+//         third: function* (welcome: string) {
+//             yield `Welcome ${welcome ? welcome : 'default'} 1`
+//             yield `Welcome ${welcome ? welcome : 'default'} 2`
+//             return `Welcome ${welcome ? welcome : 'default'} 3`
+//         }
+//     }),
+// })
 
-const calledCompose = compose({
-    inner: {
-        third: ['Alonia'],
-    }
-})
+// const calledCompose = compose({
+//     inner: {
+//         third: ['Alonia'],
+//     }
+// })
 
-log(
-    calledCompose.next()
-)
-log(
-    calledCompose.next()
-)
-log(
-    calledCompose.next()
-)
-log(
-    calledCompose.next()
-)
-log(
-    calledCompose.next()
-)
-
-
-
-
-
-function log(obj: any) {
-    console.log(inspect(obj, false, Infinity))
-}
-// console.log(
+// log(
 //     calledCompose.next()
 // )
-// console.log(
+// log(
 //     calledCompose.next()
 // )
-// console.log(
+// log(
 //     calledCompose.next()
 // )
-// console.log(
+// log(
 //     calledCompose.next()
 // )
+// log(
+//     calledCompose.next()
+// )
+
+
+
+
+
+// function log(obj: any) {
+//     console.log(inspect(obj, false, Infinity))
+// }
